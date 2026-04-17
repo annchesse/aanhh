cba43ddf9b11cf162fc29d1152688ce2a3e82045
annchesse <oseven113@gmail.com>
2026-04-17 12:30:10 +0500

    Add files via upload

---
+++ ср1.js
@@ -0,0 +1,31 @@
+const IMAGE_SRCS = [
+
+  "https://raw.githubusercontent.com/annchesse/aanhh/refs/heads/main/%D1%81%D1%801.png",
+
+  "https://raw.githubusercontent.com/annchesse/aanhh/refs/heads/main/%D1%81%D1%802.png",
+
+  "https://raw.githubusercontent.com/annchesse/aanhh/refs/heads/main/%D1%81%D1%803.png",
+
+  "https://raw.githubusercontent.com/annchesse/aanhh/refs/heads/main/%D1%81%D1%804.png",
+
+  "https://raw.githubusercontent.com/annchesse/aanhh/refs/heads/main/%D1%81%D1%805.png",
+
+  "https://raw.githubusercontent.com/annchesse/aanhh/refs/heads/main/%D1%81%D1%806.png"
+
+];
+const IMAGE_ASPECTS = [1, 1, 1, 1, 1, 1];
+const FACE_NAMES = [
+
+  "РАЗУМОВСКИЙ",
+
+  "ГЕНИЙ",
+
+  "МИЛЛИАРДЕР",
+
+  "ПЛЕЙБОЙ",
+
+  "МИЗАНТРОП",
+
+  "МЕЦЕНАТ"
+
+];
