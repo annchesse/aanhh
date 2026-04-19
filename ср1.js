2b8a9fbf24811f604d99278ae5208c0b8d139ad8
annchesse <oseven113@gmail.com>
2026-04-19 16:14:09 +0500

    Add files via upload

--- ср1.js
+++ ср1.js
@@ -1,16 +1,16 @@
 const IMAGE_SRCS = [
 
-  "https://raw.githubusercontent.com/annchesse/aanhh/refs/heads/main/%D1%81%D1%801.png",
+  "https://raw.githack.com/annchesse/aanhh/refs/heads/main/%D1%81%D1%801.png",
 
-  "https://raw.githubusercontent.com/annchesse/aanhh/refs/heads/main/%D1%81%D1%802.png",
+  "https://raw.githack.com/annchesse/aanhh/refs/heads/main/%D1%81%D1%802.png",
 
-  "https://raw.githubusercontent.com/annchesse/aanhh/refs/heads/main/%D1%81%D1%803.png",
+  "https://raw.githack.com/annchesse/aanhh/refs/heads/main/%D1%81%D1%803.png",
 
-  "https://raw.githubusercontent.com/annchesse/aanhh/refs/heads/main/%D1%81%D1%804.png",
+  "https://raw.githack.com/annchesse/aanhh/refs/heads/main/%D1%81%D1%804.png",
 
-  "https://raw.githubusercontent.com/annchesse/aanhh/refs/heads/main/%D1%81%D1%805.png",
+  "https://raw.githack.com/annchesse/aanhh/refs/heads/main/%D1%81%D1%805.png",
 
-  "https://raw.githubusercontent.com/annchesse/aanhh/refs/heads/main/%D1%81%D1%806.png"
+  "https://raw.githack.com/annchesse/aanhh/refs/heads/main/%D1%81%D1%806.png"
 
 ];
 const IMAGE_ASPECTS = [1, 1, 1, 1, 1, 1];
